46431056eb46acec5157ab92e8bfe768a4e60e79
takato-noguchi <takato.noguchi23@gmail.com>
2023-02-08 21:12:19 +0900

    first commit

---
+++ pages/posts/_id.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container py-5">
+    <b-card>
+      <b-card-text
+        class="show-txt"
+      >
+        {{ post.content }}
+      </b-card-text>
+      <b-button
+        size="sm"
+        @click="toTop()"
+      >
+        Topへ
+      </b-button>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      post: {},
+    }
+  },
+  mounted() {
+    this.fetchContent()
+  },
+  methods: {
+    fetchContent() {
+      const url = `http://localhost:3000/v1/posts/${this.$route.params.id}`
+      this.$axios.get(url)
+        .then((res) => {
+          this.post = res.data.post
+        })
+        .catch(() => {
+          this.toTop()
+        })
+    },
+  },
+  toTop() {
+    this.$router.push('/posts')
+  }
+}
+</script>
+
+<style scoped>
+.show-txt {
+  color: #000;
+}
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="container py-5 position-relative">
+    <v-card
+      v-for="post in posts" :key="post.id"
+      max-width="500"
+      class="mx-auto mb-5"
+      @click="toShow(post.id)"
+    >
+      <v-card-text>
+        {{ post.content }}
+      </v-card-text>
+    </v-card>
+
+    <b-button
+      v-b-modal.new-modal
+      class="position-absolute mt-4 action-btn"
+      pill
+      variant="primary"
+      size="lg"
+    >
+      +
+    </b-button>
+
+    <b-modal
+      hide-header
+      hide-footer
+      id="new-modal"
+    >
+      <b-form-textarea
+        v-model="content"
+        autofocus
+      />
+      <b-button
+        class="mt-3"
+        variant="primary"
+        @click="save()"
+      >
+        保存
+      </b-button>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      posts: {
+        content: ''
+      },
+    }
+  },
+  mounted() {
+    this.fetchContents()
+  },
+  computed: {
+    params() {
+      return {
+        post: {
+          content: this.content
+        }
+      }
+    }
+  },
+  methods: { 
+    fetchContents() {
+      const url = "http://localhost:3000/v1/posts"
+      this.$axios.get(url)
+        .then((res) => {
+          this.posts = res.data
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    },
+    save() {
+      const url = 'http://localhost:3000/v1/posts'
+      this.$axios.post(url, this.params)
+        .then((res) => {
+          this.content = ''
+          console.log(res)
+          this.fetchContents()
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    },
+    toShow(id) {
+      this.$router.push(`/posts/${id}`)
+    }
+  }
+}
+</script>
